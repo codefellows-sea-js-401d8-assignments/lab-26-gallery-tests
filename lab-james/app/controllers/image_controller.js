3dd27d6c4d465a85a880f1a6347978f8f00f06fc
JAMES MOON <SENDJMOON@GMAIL.COM>
2016-08-29 00:00:54 -0700

    completed test for home directive

--- lab-james/app/controllers/image_controller.js
+++ lab-james/app/controllers/image_controller.js
@@ -14,7 +14,6 @@ module.exports = function(app) {
         images: [
           {
             id: 1,
-            albumId: 1,
             title: 'Syndrome',
             description: '1337 N3RD R463.',
             url: 'http://img.lum.dolimg.com/v1/images/image_7b7a8d9b.jpeg',
@@ -22,7 +21,6 @@ module.exports = function(app) {
           },
           {
             id: 2,
-            albumId: 1,
             title: 'Lots\'O Huggin\'',
             description: 'Don\'t hug it out.',
             url: 'https://pbs.twimg.com/profile_images/1313971811/toy-story-3-lotso-bear_400x400.jpg',
@@ -30,7 +28,6 @@ module.exports = function(app) {
           },
           {
             id: 3,
-            albumId: 1,
             title: 'Sid Phillips',
             description: 'This kid needs Jesus.',
             url: 'https://pbs.twimg.com/profile_images/471625645483495424/v-JTwotY_400x400.jpeg',
@@ -38,7 +35,6 @@ module.exports = function(app) {
           },
           {
             id: 4,
-            albumId: 1,
             title: 'Lord Farquuad',
             description: 'The evilest (it\'s a word) of them all.',
             url: 'http://i1.kym-cdn.com/entries/icons/original/000/020/110/Dfdddddd.jpeg',
@@ -57,7 +53,6 @@ module.exports = function(app) {
         images: [
           {
             id: 1,
-            albumId: 2,
             title: 'Mr. Incredible',
             description: 'World\'s greatest dad.',
             url: 'http://www.ipadforums.net/wallpapers/data/508/Mr_Incredible.png',
@@ -65,7 +60,6 @@ module.exports = function(app) {
           },
           {
             id: 2,
-            albumId: 2,
             title: 'Wall-E',
             description: 'EeeeEEeeEEEeeEEva',
             url: 'http://blog.zoolert.com/wp-content/uploads/2015/08/LEGO-WALL-E-4-nwm.jpg',
@@ -73,7 +67,6 @@ module.exports = function(app) {
           },
           {
             id: 3,
-            albumId: 2,
             title: 'Buzz Lightyear',
             description: 'To infinity...and beyond!',
             url: 'https://pbs.twimg.com/profile_images/703245394/buzz_400x400.png',
@@ -81,7 +74,6 @@ module.exports = function(app) {
           },
           {
             id: 4,
-            albumId: 2,
             title: 'Rafiki',
             description: 'Best ever.',
             url: 'https://i.ytimg.com/vi/mjYGB4uYgWw/maxresdefault.jpg',
