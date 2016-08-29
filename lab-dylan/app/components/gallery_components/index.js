23202198014735b6c24841988421fe05c8fa8430
dylanjsa90 <dylanjsanders1@gmail.com>
2016-08-29 14:06:22 -0700

    renaming album

--- lab-dylan/app/components/gallery_components/index.js
+++ lab-dylan/app/components/gallery_components/index.js
@@ -4,4 +4,6 @@ module.exports = function(app) {
   require('./text_component')(app);
   require('./fullsize_component')(app);
   require('./thumbnail_component')(app);
+  require('./gallery_directive_ctrl')(app);
+  require('./gallery_directive')(app);
 };
